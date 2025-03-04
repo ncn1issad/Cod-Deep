c25fa0127b741c269ec7f12b836fef13af0f087a
ncn1 <help2921@gmail.com>
2025-03-04 20:04:37 +0200

    first commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lift.kt
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode
+
+import com.rowanmcalpin.nextftc.core.Subsystem
+import com.rowanmcalpin.nextftc.core.command.Command
+import com.rowanmcalpin.nextftc.core.control.controllers.PIDFController
+import com.rowanmcalpin.nextftc.core.control.controllers.feedforward.StaticFeedforward
+import com.rowanmcalpin.nextftc.ftc.hardware.controllables.MotorEx
+import com.rowanmcalpin.nextftc.ftc.hardware.controllables.RunToPosition
+
+object Lift : Subsystem() {
+    lateinit var motor: MotorEx
+
+    private val controller = PIDFController(0.005, 0.0, 0.0, StaticFeedforward(0.0))
+
+    private const val NAME = "lift_motor"
+
+    override fun initialize() {
+        motor = MotorEx(NAME)
+    }
+
+    fun setPower(power: Double) {
+        motor.power = power
+    }
+
+    fun setTargetPosition(position: Double) {
+        RunToPosition(motor, position, controller, this).start()
+    }
+
+    val toLow: Command
+        get() = RunToPosition(motor, 0.0, controller, this)
+}
